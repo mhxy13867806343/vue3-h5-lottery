d106ce073394306347f7ed3e4b57985df8305ab3
hooks vue <hooksvue@hooksdeMacBook-Pro.local>
2024-02-20 13:46:29 +0800

    Revert "初始化1"
    This reverts commit 351eb821145d129a76d2fefd9359f191258f101c.

--- .env.test.js
+++
@@ -1,5 +0,0 @@
-const UNI_APP = { 
-  BASE_API: 'http://cm-h5-api.haohanweb.com/cm-boot/biz/api',
-  imageUrl: "http://cm-h5-api.haohanweb.com/cm-boot/sys/common/static/",
-}
-module.exports = UNI_APP;
